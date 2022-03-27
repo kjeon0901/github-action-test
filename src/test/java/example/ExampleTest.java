a49339687c5f2970325cf80d11bf93b65421a018
Jongyoul Lee <jongyoul@gmail.com>
2022-03-28 03:35:39 +0900

    Init

---
+++ src/test/java/example/ExampleTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void firstTest() {
+        assertTrue(true);
+    }
+}
